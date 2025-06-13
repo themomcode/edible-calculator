07c1d2ee4577716a47357039edc4ba39ff5af6fb
themomcode <rachel@themomcode.com>
2025-06-13 09:41:11 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -190,6 +190,7 @@ logger.error = (msg, options) => {
 }
 
 export default defineConfig({
+	base: '/edible-calculator/',
 	customLogger: logger,
 	plugins: [
 		...(isDev ? [inlineEditPlugin(), editModeDevPlugin()] : []),
